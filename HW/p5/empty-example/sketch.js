a2bcc65141fbf9da0168c70e60cfcca0db209973
Zihan Xu <griffin.xzh@gmail.com>
2018-08-30 18:22:59 -0400

    test

---
+++ HW/p5/empty-example/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  // put setup code here
+  createCanvas(400,400);
+  background(R=200,G=100,B=20);
+  background("orange");
+  background("#123456")
+
+}
+
+function draw() {
+  // put drawing code here
+  ellipse(120,120,50,80)
+  ellipse(200,120,50,80)
+  rect(150,140,10,60)
+  rect(120,240,130,10)
+
+
+}
